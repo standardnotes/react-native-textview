f42f0bffa8942080c829ea9f699e400ab95ac90a
Radek Czemerys <radko93@gmail.com>
2020-05-04 17:59:00 +0200

    Add typescript types

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import { TextInputProps } from 'react-native';
+
+interface Props {
+  onChangeText: (text: string) => void;
+  autoFocus: boolean;
+  editable?: boolean;
+  handlesColor?: string;
+  keyboardDismissMode?: 'none' | 'on-drag' | 'interactive';
+}
+
+class TextView extends Component<Props & TextInputProps> {
+   export const focus: () => void;
+   export const blur: () => void;
+}
+
+export default TextView;
